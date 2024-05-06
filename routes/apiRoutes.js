b5c3846c449cc4c599abfe868904664596e529b3
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-05-05 20:36:43 -0600

    fixed json + added commits

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,31 @@
 const fs = require("fs");
-const uuid = require("uuid");
+const uuid = require("uuid"); // Importing the UUID library
 const router = require("express").Router();
 
-// get route for notes
+// GET route for notes
 router.get("/notes", (req, res) => {
+    // Read data from the JSON file
     const dbData = JSON.parse(fs.readFileSync("./db/db.json"));
+     // Send the data as JSON response
     res.json(dbData);
 });
 
-// post req for notes
+// POST request for notes
 router.post("/notes", (req, res) => {
+     // Read data from the JSON file
     const dbData = JSON.parse(fs.readFileSync("./db/db.json"));
+     // Create a new note object with provided title, text, and a unique id
     const newNote = {
         title: req.body.title,
         text: req.body.text,
-        id: uuid.v4(),
+        id: uuid.v4(), // Generating a unique id using UUID v4
     };
+    // Add the new note to the data array
     dbData.push(newNote);
+     // Write the updated data back to the JSON file
     fs.writeFileSync("./db/db.json", JSON.stringify(dbData));
+     // Send the updated data as JSON response
     res.json(dbData)
 });
-
-
+// Export the router
 module.exports = router
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,12 @@
-
-// require necessary packages
+// Require necessary packages
 const router = require(`express`).Router();
 const path = require(`path`)
 
-// makes a route that redirects to notes.html
+// Makes a route that redirects to notes.html when called
 router.get(`/notes`, (req, res) => {
     res.sendFile(path.join(__dirname, `../public/notes.html`))
 })
-
-// makes a route that redirects to index.html
+// Makes a route that redirects to index.html when called
 router.get(`/` , (req, res) => {
     res.sendFile(path.join(__dirname, `../public/index.html`))
 })
